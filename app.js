8a227c98567ba3410df33545f8bb31fc9d2712eb
Jon Gorman <contact@jongorman.top>
2019-09-17 09:18:35 -0500

    save

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 
+//Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/auth-boiler', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(function() {
+  console.log('Mongodb Connected')
+}).catch(err => console.log(err));
+
+//Express handlebars middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+
 //Index Route
 app.get('/', function(req, res){
-  res.send('INDEX');
+  const title = "Welcome today";
+  //Pass results into the view
+  res.render('index', {
+    title: title
+  });
 });
 
 //About Route
 app.get('/about', function(req, res){
-  res.send('ABOUT');
+  res.render('about');
 })
 
 const port = 5000;
